2a581088d0b53d718fe15cc527b3e674c39bf6f8
felixcamila <camilafelix41094@gmail.com>
2023-09-08 16:26:57 -0300

    estetica 2
    changes body and navbar

--- js/fondo.js
+++ js/fondo.js
@@ -15,3 +15,38 @@ document.addEventListener("DOMContentLoaded", function () {
         video.play(); // Iniciar la reproducción nuevamente
     }, 120000); // 120000 milisegundos = 120 segundos
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const submenus = document.querySelectorAll(".submenu");
+
+    // Ocultar todos los submenús inicialmente
+    submenus.forEach(function (submenu) {
+        submenu.style.display = "none";
+
+        // Agregar eventos mouseenter y mouseleave para mostrar/ocultar submenús
+        submenu.addEventListener("mouseenter", function () {
+            submenu.style.display = "block";
+        });
+
+        submenu.addEventListener("mouseleave", function () {
+            submenu.style.display = "none";
+        });
+    });
+
+    // Agregar eventos mouseenter y mouseleave para mostrar/ocultar submenús en los enlaces
+    const navItems = document.querySelectorAll(".nav-item");
+
+    navItems.forEach(function (navItem) {
+        const submenu = navItem.querySelector(".submenu");
+        if (submenu) {
+            navItem.addEventListener("mouseenter", function () {
+                submenu.style.display = "block";
+            });
+
+            navItem.addEventListener("mouseleave", function () {
+                submenu.style.display = "none";
+            });
+        }
+    });
+});
+
+
