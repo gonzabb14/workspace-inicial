fb014765aa69fa6548e762ff1ebe08d63c8e9df4
Bruno Cresseri <brunocresseri.1@gmail.com>
2023-09-11 01:22:06 -0300

    Experiencia del usuario en products-info

--- js/product-info.js
+++ js/product-info.js
@@ -12,38 +12,39 @@ const usuarioLogueado = localStorage.getItem('usuarioLogueado') === 'true';
     }
 
 
-function agregarImagenes() {
-    fetch("https://japceibal.github.io/emercado-api/products/" + localStorage.ProductID + ".json")
-        .then(response => {
-            if (response.ok) {
-                response.json().then(data => {
-                    let imagenes = ``;
-                    data.images.forEach(element => {
-                        imagenes += `<img style="width: 150px;" src="${element}"></img>`;
-                    });
-                    document.getElementById("imagenes").innerHTML = imagenes;
-                    //console.log(array);
-                })}
-            else {
-                console.log("Respuesta de red OK pero respuesta HTTP no OK");
-            }
-        })
-        .catch(error => {
-            console.log("Hubo un problema con la petición Fetch (agregar imagenes):" + error.message);
-        });
-}
+    function agregarImagenes() {
+        fetch("https://japceibal.github.io/emercado-api/products/" + localStorage.ProductID + ".json")
+            .then(response => {
+                if (response.ok) {
+                    response.json().then(data => {
+                        let imagenes = ``;
+                        data.images.forEach(element => {
+                            imagenes += `<img style="width: 150px;" src="${element}"></img>`;
+                        });
+                        document.getElementById("imagenes").innerHTML = imagenes;
+                        //console.log(array);
+                    })
+                }
+                else {
+                    console.log("Respuesta de red OK pero respuesta HTTP no OK");
+                }
+            })
+            .catch(error => {
+                console.log("Hubo un problema con la petición Fetch (agregar imagenes):" + error.message);
+            });
+    }
 
-function agregarComentarios() {
-    fetch("https://japceibal.github.io/emercado-api/products_comments/" + localStorage.ProductID + ".json")
-        .then(response => {
-            if (response.ok){
-                response.json().then(data => {
-                    let comentarios = ``;
-                    console.log(data);
-                    let space = " ";
+    function agregarComentarios() {
+        fetch("https://japceibal.github.io/emercado-api/products_comments/" + localStorage.ProductID + ".json")
+            .then(response => {
+                if (response.ok) {
+                    response.json().then(data => {
+                        let comentarios = ``;
+                        console.log(data);
+                        let space = " ";
                     
-                    data.forEach(comentario => {
-                        comentarios += `
+                        data.forEach(comentario => {
+                            comentarios += `
                         <div class="test-divs" style="border: 1px solid black; margin: 0px;  border: 0.5px solid #897d7d38;">
                             <div style="display: flex; flex-direction: row;">
                                 <p class="test"><strong>${comentario.user}</strong></p>
@@ -62,29 +63,29 @@ function agregarComentarios() {
                         </div>
                         `;
                         
+                        })
+                        document.getElementById("comentarios").innerHTML = comentarios;
+                        //agregarScore(comentario.score, i);
+                        let i = 0;
+                        data.forEach(comentario => {
+                            const cal = document.getElementsByClassName("calificaciones")[i].getElementsByTagName("span");
+                            for (let j = 0; j < comentario.score; j++) {
+                                cal[j].classList.add("checked");
+                            }
+                            i++;
+                        })
                     })
-                    document.getElementById("comentarios").innerHTML = comentarios;
-                    //agregarScore(comentario.score, i);
-                    let i = 0;
-                    data.forEach(comentario => {
-                        const cal = document.getElementsByClassName("calificaciones")[i].getElementsByTagName("span");
-                        for (let j = 0; j < comentario.score; j++){
-                            cal[j].classList.add("checked");
-                        }
-                        i++;
-                    })
-                })
-            } else {
-                console.log("Respuesta de red OK pero respuesta HTTP no OK");
-            }
-        })
-        .catch(error => {
-            console.log("Hubo un problema con la petición Fetch (agregar comentarios):" + error.message);
-        });
-}
+                } else {
+                    console.log("Respuesta de red OK pero respuesta HTTP no OK");
+                }
+            })
+            .catch(error => {
+                console.log("Hubo un problema con la petición Fetch (agregar comentarios):" + error.message);
+            });
+    }
 
-function agregarProducto(product, categoria) {
-    let htmlContentToAppend = `
+    function agregarProducto(product, categoria) {
+        let htmlContentToAppend = `
     <div id="container-product2">
         <div id="product-information">
             <h2>${product.name}</h2>
@@ -122,52 +123,53 @@ function agregarProducto(product, categoria) {
                 <p>Tu opinión:</p>
                 <textarea style="width:500px; height:100px;" id="comentario-hecho"></textarea>
                 <p>Tu puntuación:</p>
-                <select id="calificacion-dada" style="width:35px;">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
+                <select id="calificacion-dada" style="width:50px;">
+                    <option value="1">1★</option>
+                    <option value="2">2★</option>
+                    <option value="3">3★</option>
+                    <option value="4">4★</option>
+                    <option value="5">5★</option>
                 </select>
-                <button style="width:60px; margin-top:10px; border-radius:10%; background-color:#4343ffeb;" id="boton-enviar-datos">Enviar</button>
+                <button style="width: 0px; margin-top:10px; background-color: transparent; border:none; border-radius:10%;" id="boton-enviar-datos"></button>
             </form>
         </div>
     </div>
     `;
+
+
     
-   // console.log(comments);
+        // console.log(comments);
 
-    document.getElementById("container-product").innerHTML = htmlContentToAppend;
-    agregarImagenes();
-    agregarComentarios();
-    document.getElementById("boton-enviar-datos").addEventListener("click", (e) => {
-        e.preventDefault();
-        const comentario = document.getElementById("comentario-hecho").value;
-        const score = document.getElementById("calificacion-dada").value;
+        document.getElementById("container-product").innerHTML = htmlContentToAppend;
+        agregarImagenes();
+        agregarComentarios();
+        document.getElementById("boton-enviar-datos2").addEventListener("click", (e) => {
+            e.preventDefault();
+            const comentario = document.getElementById("comentario-hecho").value;
+            const score = document.getElementById("calificacion-dada").value;
 
-        console.log(comentario, score);
+            console.log(comentario, score);
         
-        const user = JSON.parse(localStorage.registroUsuario);
-        console.log(user.nombre);
+            const user = JSON.parse(localStorage.registroUsuario);
+            console.log(user.nombre);
 
-        const fechaActual = new Date();
+            const fechaActual = new Date();
 
-        const año = fechaActual.getFullYear(); // Obtiene el año actual
-        const mes = fechaActual.getMonth() + 1; // Obtiene el mes actual (0-11, por lo que sumamos 1)
-        const dia = fechaActual.getDate(); // Obtiene el día del mes
-        const hora = fechaActual.getHours(); // Obtiene la hora del día (0-23)
-        const minutos = fechaActual.getMinutes(); // Obtiene los minutos (0-59)
-        const segundos = fechaActual.getSeconds(); // Obtiene los segundos (0-59)
+            const año = fechaActual.getFullYear(); // Obtiene el año actual
+            const mes = fechaActual.getMonth() + 1; // Obtiene el mes actual (0-11, por lo que sumamos 1)
+            const dia = fechaActual.getDate(); // Obtiene el día del mes
+            const hora = fechaActual.getHours(); // Obtiene la hora del día (0-23)
+            const minutos = fechaActual.getMinutes(); // Obtiene los minutos (0-59)
+            const segundos = fechaActual.getSeconds(); // Obtiene los segundos (0-59)
 
         
-        //console.log();
+            //console.log();
 
-        //console.log(año+'-'+mes+'-'+dia);
 
-        document.getElementById("comentarios").innerHTML += `
+            document.getElementById("comentarios").innerHTML += `
         <div class="test-divs" style="border: 1px solid black; margin: 0px;  border: 0.5px solid #897d7d38;">
             <div style="display: flex; flex-direction: row;">
-                <p class="test"><strong>${user.nombre+ " " + user.apellido}</strong></p>
+                <p class="test"><strong>${user.nombre + " " + user.apellido}</strong></p>
                 <p style="margin: 0px 10px 0px 10px;">-</p>
                 <p>${año}-${mes < 10 ? '0' + mes : mes}-${dia < 10 ? '0' + dia : dia} ${hora < 10 ? '0' + hora : hora}:${minutos < 10 ? '0' + minutos : minutos}:${segundos < 10 ? '0' + segundos : segundos}</p>
                 <p style="margin: 0px 10px 0px 10px;">-</p>
@@ -183,46 +185,54 @@ function agregarProducto(product, categoria) {
         </div>
         `;
 
-        const estrellitas = document.getElementsByClassName("calificaciones")[document.getElementsByClassName("calificaciones").length-1].getElementsByTagName("span");
-        for (let h = 0; h < score; h++) {
-            estrellitas[h].classList.add("checked");
-        }
+            const estrellitas = document.getElementsByClassName("calificaciones")[document.getElementsByClassName("calificaciones").length - 1].getElementsByTagName("span");
+            for (let h = 0; h < score; h++) {
+                estrellitas[h].classList.add("checked");
+            }
 
-        document.getElementById("comentario-hecho").value = '';
-        document.getElementById("calificacion-dada").selectedIndex = 0;
-    })
-}
+            document.getElementById("comentario-hecho").value = '';
+            document.getElementById("calificacion-dada").selectedIndex = 0;
+        })
+    }
 
 
 
-fetch('https://japceibal.github.io/emercado-api/cats_products/' + localStorage.catID + '.json')
-    .then(response => {
-        if (response.ok) {
-            response.json().then(data => {
-                //console.log(data.products[0]);
-                let i = 0;
-                while (data.products.length !== i && data.products[i].id !== parseInt(localStorage.ProductID)) {
-                    //console.log(i);
-                    //console.log(localStorage.ProductID);
-                    //console.log(data.products[i].id);
-                    i++;
-                }
+    fetch('https://japceibal.github.io/emercado-api/cats_products/' + localStorage.catID + '.json')
+        .then(response => {
+            if (response.ok) {
+                response.json().then(data => {
+                    //console.log(data.products[0]);
+                    let i = 0;
+                    while (data.products.length !== i && data.products[i].id !== parseInt(localStorage.ProductID)) {
+                        //console.log(i);
+                        //console.log(localStorage.ProductID);
+                        //console.log(data.products[i].id);
+                        i++;
+                    }
             
-                if (i < data.products.length) {
-                    const productos = data.products[i];
-                    const categoriaNombre = data.catName;
-                    //console.log("si");
-                    agregarProducto(productos, categoriaNombre);
-                } else {
-                    console.log("no");
-                }
-            })
+                    if (i < data.products.length) {
+                        const productos = data.products[i];
+                        const categoriaNombre = data.catName;
+                        //console.log("si");
+                        agregarProducto(productos, categoriaNombre);
+                    } else {
+                        console.log("no");
+                    }
+                })
 
-        } else {
-            console.log("Respuesta de red OK pero respuesta HTTP no OK");
-        }
-    })
-    .catch(error => {
-        console.log("Hubo un problema con la petición Fetch:" + error.message);
+            } else {
+                console.log("Respuesta de red OK pero respuesta HTTP no OK");
+            }
+        })
+        .catch(error => {
+            console.log("Hubo un problema con la petición Fetch:" + error.message);
+        });
+        document.getElementById('boton-enviar-datos2').addEventListener('click', function () {
+const mensaje = document.getElementById("mensaje")
+    mensaje.innerHTML = '¡Gracias por su comentario!';
+    mensaje.style.display = 'block';
+    mensaje.classList.add('alert');
+    setTimeout(function () {
+        mensaje.style.display = 'none';
+    }, 3000);
     });
-
